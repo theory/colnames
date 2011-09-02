cbcd25a26429628260062b62449ccf4e129b3034
David E. Wheeler <david@justatheory.com>
2011-09-02 10:25:07 -0700

    Switch to pg_description in tests.
    It is unchanged all the way back to 8.2 at least.

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -4,8 +4,8 @@ BEGIN;
 \i sql/colnames.sql
 
 SELECT colnames( ROW(1, 3, 'foo') );
-SELECT colnames( ROW(c.*)::pg_cast ) FROM pg_cast c LIMIT 1;
-SELECT colnames( NULL::pg_cast );
+SELECT colnames( ROW(c.*)::pg_description ) FROM pg_description c LIMIT 1;
+SELECT colnames( NULL::pg_description );
 
 CREATE TYPE foo AS (id int, name text);
 SELECT colnames( row(1, 'foo')::foo );
